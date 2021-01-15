fb8c18ebd3051049e6fbd9af66252a4b7b15f62d
Marcin Pabich <martinoqom@gmail.com>
2021-01-15 10:36:53 +0100

    Updating TV.js to react for status changes
    Brightness of the TV CAN be 0, since it means "no backlight".

--- doc/attachments/src/tv.js
+++ doc/attachments/src/tv.js
@@ -1,71 +1,91 @@
 const common = require('./common');
 
-common.createThingFromThingDescriptionFile(WoT, "./res/semantic-tv.json", function(thing) {
-    thing.writeProperty("status", false)
-    thing.writeProperty("channel", 1)
-    thing.writeProperty("volume", 30)
-    thing.writeProperty("brightness", 0)
-})
-
-
-
-/*
-
-try {
-    // Read the Thing Description inside folder
-    fs.readFile("./res/semantic-tv.json", function(error, data) {
-        if (error) {
-            // Error trying to read file
-            console.log("[ERROR] In file reading")
-            console.log(error)
-        } else {
-            try {
-                // Parsing the readed file to an object.
-                let obj = JSON.parse(data)
-                console.log("Data parsed succesfully. Producing...")
-                
-                // Produce the thing
-                WoT.produce(obj)
-                .then(thing => {
-                    console.log("Thing created succesfully. Binding to attributes...")
-                   
-                    // Init variables
-                    status = false                          // TV is off
-                    channel = 1                             // Default channel
-                    volume = 30                             // Default volume
-                    brightness = 0                          // Since it's off
-
-                    // Setting properties
-                    this.setPropertyHandler(thing, "status", status)
-                    this.setPropertyHandler(thing, "channel", channel)
-                    this.setPropertyHandler(thing, "volume", volume)
-                    this.setPropertyHandler(thing, "brightness", brightness)
-
-                    // Finally, expose thing to public
-                    thing.expose().then(() => { 
-                        console.info(thing.getThingDescription().title + " ready"); 
-                    });
-
-                })
-                .catch(error => {
-                    // Error in promise
-                    console.log("[ERROR] In promise")
-                    console.log(error)
-                })
-
-            } catch (error) {
-                // Error when reading file
-                console.log("[ERROR] In file reading")
-                console.log(error)
-            }
-        }
-    })
-
-    
-} catch (error){
-    // Error
-    console.log("[ERROR] Generic")
-    console.log(error)
+// Reference to local variable
+let localRef = {
+    status: {
+        name: "status",
+        value: false,
+        isPendingRequest: false
+    },
+    channel: {
+        name: "channel",
+        value: null,
+        default: 1,
+        off: null
+    },
+    volume: {
+        name: "volume",
+        value: 0,
+        default: 30,
+        off: 0
+    },
+    brightness: {
+        name: "brightness",
+        value: 0,
+        default: 50,
+        off: 0
+    },
+    thing: null
 }
 
-*/
+
+common.createThingFromThingDescriptionFile(WoT, "./res/semantic-tv.json", function(thing) {
+    // Save reference to the thing
+    localRef.thing = thing
+
+    // Set write/read properties
+    localRef.thing.writeProperty(localRef.status.name, localRef.status.value)
+    localRef.thing.writeProperty(localRef.channel.name, localRef.channel.value)
+    localRef.thing.writeProperty(localRef.volume.name, localRef.volume.value)
+    localRef.thing.writeProperty(localRef.brightness.name, localRef.brightness.value)
+
+    // Observing status
+    localRef.thing.observeProperty(localRef.status.name, async(data) => {
+        reactoToStatusChange(data)
+    })
+
+    // In case of TV, brightness CAN be 0 since it means "no backlight"; still it should be visible
+})
+
+async function reactoToStatusChange(data) {
+    if (localRef.status.isPendingRequest == false) {
+        localRef.status.isPendingRequest = true
+        if (data != localRef.status.value) {
+            // If it was off and now it's true, I need to turn on all params
+            if (status) {
+                Promise.all([
+                    writeProperty(localRef.channel.name, localRef.channel.default),
+                    writeProperty(localRef.volume.name, localRef.volume.default),
+                    writeProperty(localRef.brightness.name, localRef.brightness.default)
+                ]).then((values) => {
+                    localRef.status.value = data
+                    localRef.status.isPendingRequest = false
+                    return
+                })
+            } else {
+                // If it was on and now it's false, I need to turn off all params
+                Promise.all([
+                    writeProperty(localRef.channel.name, localRef.channel.off),
+                    writeProperty(localRef.volume.name, localRef.volume.off),
+                    writeProperty(localRef.brightness.name, localRef.brightness.off)
+                ]).then((values) => {
+                    localRef.status.value = data
+                    localRef.status.isPendingRequest = false
+                    return
+                })
+            }
+        }
+    }
+}
+
+function writeProperty(name, value) {
+    // Write object locally
+    for (const key of Object.keys(localRef)) {
+        if (key == name) {
+            localRef[key].value = value
+        }
+    }
+
+    // Wirte object remotely
+    return localRef.thing.writeProperty(name, value)
+}
