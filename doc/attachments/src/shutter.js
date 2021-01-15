8df0834fa76a901c05c81debac1d561fe78f71e9
Marcin Pabich <martinoqom@gmail.com>
2021-01-15 17:18:07 +0100

    Adding Shutter almost-working implementation
    ToDo: when changing level from an "undertreshold one" to "abovetreshold" one, level is automatically set to 100 due to isOpen property observe.

--- doc/attachments/src/shutter.js
+++ doc/attachments/src/shutter.js
@@ -1,47 +1,97 @@
 const common = require('./common');
 
+// Reference to local variable
+let localRef = {
+    isOpen: {
+        name: "is-open",
+        value: false,
+    },
+    openessTreshold: {
+        name: "openess-treshold",
+        value: 20,
+        default: 20,
+    },
+    level: {
+        name: "level",
+        value: 0,
+        minValue: 0,            // Fully closed
+        maxValue: 100           // Fully opened
+    },
+
+    thing: null
+}
+
 common.createThingFromThingDescriptionFile(WoT, "./res/semantic-shutter.json", function(thing) {
-    thing.writeProperty("is-open", false)
-    thing.writeProperty("openess-treshold", 20)
-    thing.writeProperty("level", 0)
+    // Save reference to the thing
+    localRef.thing = thing
+
+    // Set read/write properties
+    localRef.thing.writeProperty(localRef.isOpen.name, localRef.isOpen.value)
+    localRef.thing.writeProperty(localRef.openessTreshold.name, localRef.openessTreshold.value)
+    localRef.thing.writeProperty(localRef.level.name, localRef.level.value)
+
+    localRef.thing.observeProperty(localRef.isOpen.name, async(data) => {
+        let level = await localRef.thing.readProperty(localRef.level.name)
+        
+        if (data) {
+            // Shutter should be open at maximum only if level is currently not
+            if (level < localRef.level.maxValue) {
+                writeProperty(localRef.level.name, localRef.level.maxValue)
+            }
+        } else {
+            // Shutter should be closed at threshold only if leves is currently not
+            let currentTreshold = await localRef.thing.readProperty(localRef.openessTreshold.name)
+            if (level > currentTreshold) {
+                writeProperty(localRef.level.name, currentTreshold)
+            }  
+        }
+    })
+
+    // Observe treshold changes
+    localRef.thing.observeProperty(localRef.openessTreshold.name, async(data) => {
+        let isOpen = await localRef.thing.readProperty(localRef.isOpen.name)
+        let level = await localRef.thing.readProperty(localRef.level.name)
+        checkShutterOpeness(level, data, isOpen)
+    })
+
+    // Observe level changes
+    localRef.thing.observeProperty(localRef.level.name, async(data) => {
+        let isOpen = await localRef.thing.readProperty(localRef.isOpen.name)
+        let treshold = await localRef.thing.readProperty(localRef.openessTreshold.name)
+        checkShutterOpeness(data, treshold, isOpen)
+    })
+
+    // Move it
+    localRef.thing.setActionHandler("move", async(params) => {
+        let level = await localRef.thing.readProperty(localRef.level.name)
+        level += params.level
+        return localRef.thing.writeProperty(localRef.level.name, level)
+    })
+
 })
 
-
-/*
-function updateStatusOnIsOpen() {
-    // If isOpen == true shutter is fully opened (100)
-    if (isOpen) { level = 100 } 
-    // If isOpen == false shutter is closed (=openessTreshold)
-    else { level = openessTreshold }
-}
-
-function updateStatusOnLevel(thing) {
-    thing.readProperty("level").then((value) => {
-        if (value <= openessTreshold) {
-            isOpen = false
-        } else {
-            isOpen = true
+async function checkShutterOpeness(currentLevel, levelTreshold, isOpen) {
+    if (currentLevel > levelTreshold) {
+        if (!isOpen) {
+            // If current level (30) is higher than treshold (10) and shutter is NOT open, go and open it
+            writeProperty(localRef.isOpen.name, true)
         }
-    })
-
-    updateData(thing)
-}
-
-function updateStatusOnOpenessTreshold(thing) {
-    thing.readProperty("openess-treshold").then((value) => {
-        if (level <= value) {
-            isOpen = false
-        } else {
-            isOpen = true
+    } else if (currentLevel < levelTreshold) {
+        if (isOpen) {
+            // If current level (15) is lower than treshold (30) and shutters IS open, go and close it
+            writeProperty(localRef.isOpen.name, false)
         }
-    })
-
-    updateData(thing)
+    }
 }
 
-function updateData(thing) {
-    thing.writeProperty("isOpen", isOpen)
-    thing.writeProperty("openessTreshold", openessTreshold)
-    thing.writeProperty("level", level)
+function writeProperty(name, value) {
+    // Write object locally
+    for (const key of Object.keys(localRef)) {
+        if (key == name) {
+            localRef[key].value = value
+        }
+    }
+
+    // Wirte object remotely
+    return localRef.thing.writeProperty(name, value)
 }
-*/
